08ccacd07b9721eaa8fc4f8f43705441368cf3fb
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-24 03:22:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,13 +10,14 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  #  i = 0
-  #  while i < directors_database[:names].count do
-  #    puts directors_database[:names][:movies][i][:title].to_s
-  #  end
 # pp directors_database
 # get AoHoAoH
-# directors_database[names = 0 ][movies][title]
-# need directors_database[0][:movies][****ALL TITLES****]
-  puts directors_database[0][:movies][0][:title]
+# directors_database[director name index][movies][movie index][title]
+# need directors_database[0][:movies][index][:title]
+
+  movie_index = 0
+
+  while condition movie_index < directors_database[0][:movies].count do
+    puts directors_database[0][:movies][movie_index][:title]
+  end
 end
