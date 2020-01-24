8eb718c2d2dc1be26c54d66e55b22aca37182b0f
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-24 02:47:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-   directors_database[0].each_key { |key| puts key }
+   directors_database[.each_key { |key| puts key }
 
 
 end
